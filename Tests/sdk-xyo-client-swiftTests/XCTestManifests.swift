0dada4ec91f2d7e2a5b179d8d8324d501b0c9d63
Arie Trouw <arie.trouw@xy.company>
2021-04-25 15:00:58 -0700

    format

--- Tests/sdk-xyo-client-swiftTests/XCTestManifests.swift
+++ Tests/sdk-xyo-client-swiftTests/XCTestManifests.swift
@@ -2,8 +2,8 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(sdk_xyo_client_swiftTests.allTests),
-    ]
+  return [
+    testCase(sdk_xyo_client_swiftTests.allTests),
+  ]
 }
 #endif
