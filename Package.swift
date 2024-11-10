0aeb7f7418f49f1795c8e30a106ed1bcafe6c633
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-10 13:17:15 -0600

    Bump dep version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     .package(
       name: "secp256k1",
       url: "https://github.com/21-DOT-DEV/swift-secp256k1",
-      "0.17.0"..."0.17.0"),
+      "0.18.0"..."0.18.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
