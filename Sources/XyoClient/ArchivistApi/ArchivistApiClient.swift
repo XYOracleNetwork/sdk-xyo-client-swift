dcbf4507a9100901f40cf5facd5bcd02440c6b10
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-12 17:55:37 -0600

    Remove unused code

--- Sources/XyoClient/ArchivistApi/ArchivistApiClient.swift
+++ Sources/XyoClient/ArchivistApi/ArchivistApiClient.swift
@@ -1,11 +1,6 @@
 import Alamofire
 import Foundation
 
-public struct XyoApiBoundWitnessBody: Encodable {
-  var boundWitnesses: [XyoBoundWitnessJson]
-  var payloads: [XyoPayload]?
-}
-
 public class XyoArchivistApiClient {
 
   private static let ArchivistInsertQuerySchema = "network.xyo.query.archivist.insert"
@@ -58,58 +53,6 @@ public class XyoArchivistApiClient {
     let decodedResponse = try JSONDecoder().decode(ModuleQueryResult.self, from: responseData)
 
     return decodedResponse.payloads
-  }
-
-  public func postBoundWitnesses(
-    _ entries: [XyoBoundWitnessJson]
-  ) throws {
-    try self.postBoundWitnesses(entries) { _ in }
-  }
-
-  public func postBoundWitnesses(
-    _ entries: [XyoBoundWitnessJson],
-    _ closure: @escaping (_ error: String?) -> Void
-  ) throws {
-    let body = entries
-    AF.request(
-      self.url,
-      method: .post,
-      parameters: body,
-      encoder: JSONParameterEncoder.default
-    ).validate().responseData(queue: XyoArchivistApiClient.queue) { response in
-      switch response.result {
-      case .failure:
-        XyoArchivistApiClient.mainQueue.async {
-          if let data = response.data {
-            closure(String(decoding: data, as: UTF8.self))
-          } else {
-            closure("Unknown Error")
-          }
-        }
-
-      case .success:
-        XyoArchivistApiClient.mainQueue.async {
-          closure(nil)
-        }
-      }
-    }
-  }
-
-  public func postBoundWitness(
-    _ entry: XyoBoundWitnessJson
-  ) throws {
-    try self.postBoundWitnesses([entry]) { _ in }
-  }
-
-  public func postBoundWitness(
-    _ entry: XyoBoundWitnessJson,
-    _ closure: @escaping (_ error: String?) -> Void
-  ) throws {
-    try self.postBoundWitnesses([entry], closure)
-  }
-
-  public static func get(_ config: XyoArchivistApiConfig) -> XyoArchivistApiClient {
-    return XyoArchivistApiClient(config, Account())
   }
 }
 
