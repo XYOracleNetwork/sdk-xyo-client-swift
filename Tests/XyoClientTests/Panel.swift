e455164800d6db379ff0686c4ec41fc2ee982b30
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-11 19:43:22 -0600

    Async report test

--- Tests/XyoClientTests/Panel.swift
+++ Tests/XyoClientTests/Panel.swift
@@ -22,6 +22,25 @@ final class PanelTests: XCTestCase {
     XCTAssertNotNil(panel)
   }
 
+  func testAsyncReport() async throws {
+    let apiDomain = XyoPanel.Defaults.apiDomain
+    let archive = XyoPanel.Defaults.apiModule
+    _ = XyoAddress()
+    let witness = XyoBasicWitness(observer: {
+      let payload = XyoPayload("network.xyo.basic")
+      return payload
+    })
+    let panel = XyoPanel(
+      archive: archive, apiDomain: apiDomain, witnesses: [witness, XyoSystemInfoWitness()])
+
+    // Act: Call the report method
+    let result = try await panel.report()
+    XCTAssertTrue(result.isEmpty, "Expected empty result from report for readonly SDK")
+    // TODO: Deserialize the response
+    // XCTAssertFalse(result.isEmpty, "Expected non-empty result from report")
+    // XCTAssertEqual(result.count, 1, "Expected one payload in the result")
+  }
+
   func testPanelReport() throws {
     let apiDomain = XyoPanel.Defaults.apiDomain
     let archive = XyoPanel.Defaults.apiModule
