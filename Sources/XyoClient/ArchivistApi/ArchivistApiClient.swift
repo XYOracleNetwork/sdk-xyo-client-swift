1f3860ed01e9d60ffae0cbc5973afab8a554cc0a
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-11 18:11:13 -0600

    Add module query result type

--- Sources/XyoClient/ArchivistApi/ArchivistApiClient.swift
+++ Sources/XyoClient/ArchivistApi/ArchivistApiClient.swift
@@ -6,7 +6,32 @@ public struct XyoApiBoundWitnessBody: Encodable {
   var payloads: [XyoPayload]?
 }
 
+struct ModuleQueryResult: Encodable {
+  let bw: XyoBoundWitnessJson
+  let payloads: [XyoPayload]
+  let errors: [XyoPayload]
+  init(bw: XyoBoundWitnessJson, payloads: [XyoPayload] = [], errors: [XyoPayload] = []) {
+    self.bw = bw
+    self.payloads = payloads
+    self.errors = errors
+  }
+  func encode(to encoder: Encoder) throws {
+    // Create an unkeyed container for array encoding
+    var container = encoder.unkeyedContainer()
+    // Encode `bw` as the first element
+    try container.encode(bw)
+    // Encode `payloads` as the second element
+    try container.encode(payloads)
+    // Encode `errors` as the third element
+    try container.encode(errors)
+  }
+}
+
 public class XyoArchivistApiClient {
+
+  private static let ArchivistInsertQuerySchema = "network.xyo.query.archivist.insert"
+  private static let ArchivistInsertQuery: XyoPayload = XyoPayload(ArchivistInsertQuerySchema)
+
   let config: XyoArchivistApiConfig
 
   public var authenticated: Bool {
@@ -30,13 +55,18 @@ public class XyoArchivistApiClient {
     self.config = config
   }
 
-  public func insert(payloads: [XyoBoundWitnessJson]) async throws -> XyoBoundWitnessJson {
-    // TODO: Build query bound witness
+  public func insert(payloads: [XyoPayload]) async throws -> XyoBoundWitnessJson {
+    // Build QueryBoundWitness
+    let (bw, signed) = try BoundWitnessBuilder()
+      .payloads(payloads)
+      .query(XyoArchivistApiClient.ArchivistInsertQuery)
+      .build()
+
     // Perform the request and await the result
     let responseData = try await AF.request(
       self.url,
       method: .post,
-      parameters: payloads,
+      parameters: ModuleQueryResult(bw: bw, payloads: signed),
       encoder: JSONParameterEncoder.default
     )
     .validate()
@@ -44,7 +74,7 @@ public class XyoArchivistApiClient {
     .value
 
     // Attempt to decode the response data into XyoBoundWitnessJson
-    let decodedResponse = try JSONDecoder().decode([XyoBoundWitnessJson].self, from: responseData)
+      let decodedResponse = try JSONDecoder().decode(ModuleQueryResult.self, from: responseData)
     // TODO: Return payloads instead once they're deserializable
     return decodedResponse[0]
   }
