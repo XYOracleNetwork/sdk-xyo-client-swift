807d5246a6d4fdcc0c4086693c548d3b460b0175
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-14 16:02:05 -0600

    Implement async

--- Sources/XyoClient/Witness/AbstractWitness.swift
+++ Sources/XyoClient/Witness/AbstractWitness.swift
@@ -1,4 +1,5 @@
 import Foundation
+
 open class AbstractWitness: AbstractModule, Witness {
     open func observe() -> [XyoPayload] {
         preconditionFailure("This method must be overridden")
@@ -9,16 +10,17 @@ open class AbstractAsyncWitness: AbstractModule, WitnessAsync {
     open func observe(completion: @escaping ([XyoPayload]?, Error?) -> Void) {
         preconditionFailure("This method must be overridden")
     }
+
     @available(iOS 15, *)
     open func observe() async throws -> [XyoPayload] {
         try await withCheckedThrowingContinuation { continuation in
             observe { payloads, error in
-                if let payloads = payloads {
-                    continuation.resume(returning: payloads)
-                } else if let error = error {
+                if let error = error {
                     continuation.resume(throwing: error)
+                } else if let payloads = payloads {
+                    continuation.resume(returning: payloads)
                 } else {
-                    continuation.resume(throwing: NSError(domain: "UnknownError", code: 0, userInfo: nil))
+                    continuation.resume(returning: [])
                 }
             }
         }
--- Sources/XyoClient/Witness/Witness.swift
+++ Sources/XyoClient/Witness/Witness.swift
@@ -4,4 +4,7 @@ public protocol Witness {
 
 public protocol WitnessAsync {
     func observe(completion: @escaping ([XyoPayload]?, Error?) -> Void)
+
+    @available(iOS 15, *)
+    func observe() async throws -> [XyoPayload]
 }
