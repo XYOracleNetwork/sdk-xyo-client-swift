2fd9e6661916b4b9f9b11acacd878028e51cb019
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-13 18:01:16 -0600

    Update platforms to support async

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
   name: "XyoClient",
   platforms: [
-    .macOS(.v10_15),
-    .iOS(.v12),
-    .tvOS(.v12),
-    .watchOS(.v5),
+    .macOS(.v12),
+    .iOS(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8),
   ],
   products: [
     .library(
