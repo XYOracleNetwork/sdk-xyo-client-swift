0158b0c873c812283ec1b3c0138eeb6be493f9c0
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-14 15:58:34 -0600

    Auto implement async for consumers

--- Sources/XyoClient/Witness/AbstractWitness.swift
+++ Sources/XyoClient/Witness/AbstractWitness.swift
@@ -1,3 +1,4 @@
+import Foundation
 open class AbstractWitness: AbstractModule, Witness {
     open func observe() -> [XyoPayload] {
         preconditionFailure("This method must be overridden")
@@ -10,6 +11,16 @@ open class AbstractAsyncWitness: AbstractModule, WitnessAsync {
     }
     @available(iOS 15, *)
     open func observe() async throws -> [XyoPayload] {
-        preconditionFailure("This method must be overridden")
+        try await withCheckedThrowingContinuation { continuation in
+            observe { payloads, error in
+                if let payloads = payloads {
+                    continuation.resume(returning: payloads)
+                } else if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(throwing: NSError(domain: "UnknownError", code: 0, userInfo: nil))
+                }
+            }
+        }
     }
 }
