07ff90e88027df6c42226bc250abbbeb9bd60746
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-04 14:48:48 -0600

    Add tests for uncompressed public keys

--- Tests/XyoClientTests/WalletVectors.swift
+++ Tests/XyoClientTests/WalletVectors.swift
@@ -3,23 +3,24 @@ import XCTest
 @testable import XyoClient
 
 struct TestVector: Decodable {
-    let phrase: String
-    let path: String
     let address: String
+    let path: String
+    let phrase: String
     let privateKey: String
     let publicKey: String
+    let publicKeyUncompressed: String
 }
 
 let phrase = "later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief"
 
 let testVectors: [TestVector] = [
     .init(
-        phrase: phrase,
-        path: "m/44'/60'/0'/0/0",
         address: "e46c258c74c7c1df33d7caa4c2c664dc0843ab3f",
+        path: "m/44'/60'/0'/0/0",
+        phrase: phrase,
         privateKey: "96a7705eedbb701a03ee235911253fd3eb80e48a06106c0bf957d42b72bd8efa",
-        //        publicKey: "a9f10779cb44e73a1983b8225ce9de96ff63cbc8a2900db102fa55a38a14b206f850a6decf0d0277c8ea237d865a06b6237f07eaf4273217ed6b2ed830161bef"
-        publicKey: "03a9f10779cb44e73a1983b8225ce9de96ff63cbc8a2900db102fa55a38a14b206"
+        publicKey: "03a9f10779cb44e73a1983b8225ce9de96ff63cbc8a2900db102fa55a38a14b206",
+        publicKeyUncompressed: "04a9f10779cb44e73a1983b8225ce9de96ff63cbc8a2900db102fa55a38a14b206f850a6decf0d0277c8ea237d865a06b6237f07eaf4273217ed6b2ed830161bef"
     )
 ]
 
@@ -33,6 +34,7 @@ class WalletVectorTests: XCTestCase {
             XCTAssertEqual(wallet.privateKey?.toHex(), vector.privateKey)
             XCTAssertEqual(wallet.address?.toHex(), vector.address)
             XCTAssertEqual(wallet.publicKey?.toHex(), vector.publicKey)
+            XCTAssertEqual(wallet.publicKeyUncompressed?.toHex(), vector.publicKeyUncompressed)
 
             let entropy = try Bip39.mnemonicToEntropy(phrase: vector.phrase)
             let seed = try Bip39.entropyToSeed(entropy: entropy)
@@ -42,6 +44,7 @@ class WalletVectorTests: XCTestCase {
             XCTAssertEqual(wallet2.privateKey?.toHex(), vector.privateKey)
             XCTAssertEqual(wallet2.address?.toHex(), vector.address)
             XCTAssertEqual(wallet2.publicKey?.toHex(), vector.publicKey)
+            XCTAssertEqual(wallet2.publicKeyUncompressed?.toHex(), vector.publicKeyUncompressed)
         } catch {
             print("\nCaught error: \(error)\n")
             XCTAssertTrue(false)
