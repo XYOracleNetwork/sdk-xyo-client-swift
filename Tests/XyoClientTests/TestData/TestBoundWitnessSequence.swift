3787f2899b148c73d179eb111d7f5868bcf1fb28
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-05 05:55:27 -0600

    BW Sequence test cases

---
+++ Tests/XyoClientTests/TestData/TestBoundWitnessSequence.swift
@@ -0,0 +1,49 @@
+import XyoClient
+
+public struct BoundWitnessSequenceTestCase {
+    public var privateKeys: [String]
+    public var addresses: [Address]
+    public var payloads: [EncodablePayloadInstance]
+    public var payloadHashes: [String]
+    public var previousHashes: [String?]
+}
+
+public struct PayloadsWithHashes {
+    public var payloads: [EncodablePayloadInstance]
+    public var payloadHashes: [String]
+}
+
+let payloadSequences: [PayloadsWithHashes] = [
+    .init(payloads: [IdPayload(0)], payloadHashes: [""]),
+    .init(payloads: [IdPayload(1)], payloadHashes: [""]),
+    .init(payloads: [IdPayload(2), IdPayload(3)], payloadHashes: ["", ""]),
+
+]
+
+let boundWitnessSequenceTestCase1: BoundWitnessSequenceTestCase = .init(
+    privateKeys: [],
+    addresses: [],
+    payloads: payloadSequences[0].payloads,
+    payloadHashes: payloadSequences[0].payloadHashes,
+    previousHashes: []
+)
+
+let boundWitnessSequenceTestCase2: BoundWitnessSequenceTestCase = .init(
+    privateKeys: [],
+    addresses: [],
+    payloads: payloadSequences[1].payloads,
+    payloadHashes: payloadSequences[1].payloadHashes,
+    previousHashes: []
+)
+
+let boundWitnessSequenceTestCase3: BoundWitnessSequenceTestCase = .init(
+    privateKeys: [],
+    addresses: [],
+    payloads: payloadSequences[2].payloads,
+    payloadHashes: payloadSequences[2].payloadHashes,
+    previousHashes: []
+)
+
+let boundWitnessSequenceTestCases = [
+    boundWitnessSequenceTestCase1, boundWitnessSequenceTestCase2, boundWitnessSequenceTestCase3,
+]
