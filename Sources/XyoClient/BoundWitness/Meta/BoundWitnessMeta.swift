a876ff63353cd52fa84ec8e9cad4a2b3745a20e9
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-10 07:23:06 -0600

    custom encoding on BW meta

--- Sources/XyoClient/BoundWitness/Meta/BoundWitnessMeta.swift
+++ Sources/XyoClient/BoundWitness/Meta/BoundWitnessMeta.swift
@@ -24,4 +24,11 @@ public class BoundWitnessMeta: EncodableEmptyMeta, BoundWitnessMetaProtocol, Dec
         client = try values.decode(String.self, forKey: .client)
         signatures = try values.decode([String].self, forKey: .signatures)
     }
+
+    override public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(self.client, forKey: .client)
+        try container.encode(self.signatures, forKey: .signatures)
+    }
 }
